e68e18ce18cff01687a0b99c07543f1297af1565
avimehenwal <avi.mehanwal@gmail.com>
2019-09-16 21:20:33 +0200

    Script to build and run tng-bench quickly #16

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# AUTHOR  : avimehenwal
+# DATE    : 16th-Sep-2019
+# PURPOSE : Quickly build and launch tng-bench commandline 
+
+
+IMG_NAME="UPB/tng-bench"
+
+if [ `basename $PWD` == "tng-sdk-benchmark" ]
+then
+    docker build --tag $IMG_NAME .
+    docker run \
+        --interactive --rm --tty \
+        --volume `pwd`:/tng-sdk-benchmark \
+        $IMG_NAME
+fi
