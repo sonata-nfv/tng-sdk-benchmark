a24799e0f7df691618a002671f640346ba291c25
avimehenwal <avi.mehanwal@gmail.com>
2019-10-24 16:12:05 +0200

    Add script to add osm client environment variables

---
+++ osm-environment-variables.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# AUTHOR    : Avi Mehenwal
+# DATE      : 24-october-2019
+# PURPOSE   : set environment variables for OSM Client
+#
+# source osm-environment-variables.sh
+
+
+export OSM_USER='admin'
+export OSM_HOSTNAME=fgcn-backflip3.cs.upb.de
+
+# END
