76dae036daf0326e2e635f345c874500a683b7a5
harshita <er.harshitapuri@gmail.com>
2019-10-28 23:05:10 +0000

    Corrected an error in node-exporter installer script

--- prometheus/node_exporter_installer.sh
+++ prometheus/node_exporter_installer.sh
@@ -5,7 +5,7 @@ wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_
 tar -xvf node_exporter-0.18.1.linux-amd64.tar.gz
 mv node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
 useradd -rs /bin/false node_exporter
-echo "[Unit]\nDescription=Node Exporter\nAfter=network.target\n\n\n[Service]\nUser=node_exporter\nGroup=node_exporter\nType=simple\nExecStart=/usr/local/bin/node_exporter\n" >> /etc/systemd/system/node_exporter.service
+printf "[Unit]\nDescription=Node Exporter\nAfter=network.target\n\n[Service]\nUser=node_exporter\nGroup=node_exporter\nType=simple\nExecStart=/usr/local/bin/node_exporter\n\n[Install]\nWantedBy=multi-user.target\n" > /etc/systemd/system/node_exporter.service
 systemctl daemon-reload
 systemctl start node_exporter
 systemctl status node_exporter
